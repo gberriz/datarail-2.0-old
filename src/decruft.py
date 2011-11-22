ea9b488b5ef67e178f8ad8c1d915661975e90d38
Gabriel F. Berriz <gberriz@hms.harvard.edu>
2011-11-22 17:12:32 -0500

    output stream customization

--- src/decruft.py
+++ src/decruft.py
@@ -1,3 +1,5 @@
+import sys
+
 REGISTRY = dict()
 
 def register(path, globs):
@@ -5,7 +7,23 @@ def register(path, globs):
     REGISTRY[path] = Decrufter(set(globs.keys()))
 
 class Decrufter(object):
-    def __init__(self, globs):
+    '''
+# ... IMPORTS
+
+from decruft import Decrufter
+DECRUFTER = Decrufter(globals())
+
+# ... BODY OF MODULE
+
+DECRUFTER.instrument(globals())
+
+if __name__ == '__main__':
+    import sys
+    DECRUFTER.exit(main(sys.argv)) # any output sent to sys.stderr by default
+    '''
+
+    def __init__(self, globs, out=sys.stderr):
+        self.out = out
         self.start = set(globs.keys())
         self.ncalls = dict()
 
@@ -15,11 +33,12 @@ class Decrufter(object):
         for k, v in globs.items():
             if (k in start) or (not hasattr(v, '__call__')):
                 continue
-            # print 'wrapping %s: %s' % (k, hasattr(v, '__call__')),
+            # out = self.out
+            # print >> out, 'wrapping %s: %s' % (k, hasattr(v, '__call__')),
             # try:
-            #     print ' (%s)' % v
+            #     print >> out, ' (%s)' % v
             # except Exception, e:
-            #     print '*** %s (%s) ***' % (e.__class__.__name__, e)
+            #     print >> out, '*** %s (%s) ***' % (e.__class__.__name__, e)
             globs[k] = self._wrap(k, v)
 
 
@@ -34,7 +53,13 @@ class Decrufter(object):
 
     def report(self):
         ncalls = self.ncalls
+        out = self.out
         for k, v in ncalls.items():
             if v == 0:
-                print k
+                print >> out, k
 
+    # Note: 0 is Python's official default return value for
+    # __builtins__.exit()
+    def exit(self, val=0):
+        self.report()
+        __builtins__['exit'](val)
