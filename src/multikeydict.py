706b8ed9dbe7bd8e9bfa55a871c5fd3cc308db24
Gabriel F. Berriz <gberriz@hms.harvard.edu>
2011-11-22 14:14:15 -0500

    _dimvals and __dimvals helper methods
    ...for MultiKeyDict; useful when working with MKD models of datacubes;
    maybe they should be moved to a different module (e.g. a subclass of
    MKD specifically designed to model datacubes); ditto for all the
    stuff committed in 07b6198d1475c7f822bc0f532058aa533e64560c

--- src/multikeydict.py
+++ src/multikeydict.py
@@ -246,3 +246,16 @@ class MultiKeyDict(defaultdict):
                     yield ck + kk
             else:
                 yield ck
+
+
+    def __dimvals(self):
+        def _dv(x):
+            return x.__dimvals() if isinstance(x, MultiKeyDict) else set()
+        chs = map(_dv, self.values())
+        if all(len(ch) == 0 for ch in chs):
+            return [self._keyorder]
+        return [self._keyorder] + [sum(k, OrderedSet()) for k in zip(*chs)]
+
+
+    def _dimvals(self):
+        return map(tuple, self.__dimvals())
